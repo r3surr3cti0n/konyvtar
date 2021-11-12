b6e4d49a6d6f6aafb07716ec1d7bb230593c27b9
r3surr3cti0n <r3surr3cti0n@protonmail.com>
2021-11-12 18:38:00 +0100

    Create parseUploadedFile function

--- parts/func.php
+++ parts/func.php
@@ -83,4 +83,72 @@ class func extends DBH
             exit();
         }
     }
+
+    public function parseUploadedFile($file, $fileExt)
+    {
+        $file = "../fajlok/{$file["name"]}";
+
+        try {
+            // Create reader based on the file's extension.
+            $reader = ($fileExt == "xlsx") ? \PhpOffice\PhpSpreadsheet\IOFactory::createReader("Xlsx") : \PhpOffice\PhpSpreadsheet\IOFactory::createReader("Xls");
+            $reader->setReadDataOnly(true);
+            $reader->setReadEmptyCells(false);  // Avoid reading blank cells
+            $spreadsheet = $reader->load($file);
+            $worksheet = $spreadsheet->getActiveSheet();
+
+            $rows = [];
+            foreach ($worksheet->getRowIterator() as $row) {
+                $cellIterator = $row->getCellIterator();
+                // Loop through cells that has value.
+                $cellIterator->setIterateOnlyExistingCells(TRUE);
+
+                $cells = [];
+                foreach ($cellIterator as $cell) {
+                    $cells[] = $cell->getValue();
+                }
+                $rows[] = $cells;
+            }
+
+            // Go through the $rows and create a query from them
+            $q = "INSERT IGNORE INTO konyv(k_rakjel, k_cutszam, k_biblio) VALUES";
+
+            $i = 0;
+            $length = count($rows);
+            foreach ($rows as $line) {
+                $i++;
+
+                // Skip the first row
+                if ($i == 1) {
+                    continue;
+                }
+
+                $rak_jel = $line[0];
+                $cut_szam = $line[1];
+                $biblio = $line[2];
+
+                // If this is not the last row just separate it
+                // otherwise close it down.
+                if ($i != $length) {
+                    $q .= "($rak_jel, '$cut_szam', '$biblio'),";
+                } else {
+                    $q .= "($rak_jel, '$cut_szam', '$biblio');";
+                }
+            }
+        } catch (\PhpOffice\PhpSpreadsheet\Reader\Exception $e) {
+            $_SESSION["error"] = "Hiba történt a fájl betöltése közben!";
+            header("location: index.php");
+            exit();
+        }
+
+        $stmt = $this->connect()->prepare($q);
+        if ($this->checkExe($stmt)) {
+            $_SESSION["info"] = "{$stmt->rowCount()} db könyv került hozzáadásra!";
+            // Delete the file after we done
+            unlink("../fajlok/{$file}");
+        } else {
+            $_SESSION["error"] = "Hiba történt a könyvek hozzáadása során!";
+            header("location: index.php");
+            exit();
+        }
+    }
 }
