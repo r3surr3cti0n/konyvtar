e2e268369efeb382dd3ae273158059b0dbe51b11
r3surr3cti0n <r3surr3cti0n@protonmail.com>
2021-11-26 16:38:06 +0100

    Style using .wrapper

--- profil/index.php
+++ profil/index.php
@@ -30,45 +30,45 @@ $func = new func();
     <main id="main">
         <?php
         require_once '../parts/nav.php';
-
-        echo "<h1 class='title'>Üdvözöllek {$_SESSION["email"]}!</h1>";
-
-        if (isset($_POST["submit"])) {
-            $pwd1 = $_POST["pwd1"];
-            $pwd2 = $_POST["pwd2"];
-
-            if ($func->comparePwds($pwd1, $pwd2)) {
-                if (!$func->isNewPwd($pwd1, $_SESSION["email"])) {
-                    echo "<div id='error'>Kérem adjon meg egy ÚJ jelszót!</div>";
-                } else {
-                    if (!$func->changePwd($pwd1, $_SESSION["email"])) {
-                        echo "<div id='error'>Hiba történt!</div>";
-                    } else {
-                        echo "<div><p>Sikeresen megváltoztatta a jelszavát!</p>";
-                        header("refresh:10;url=../parts/logout.php");
-                        echo "<p>A rendszer 10 másodpercen belül kifogja léptetni!</p></div>";
-                    }
-                }
-            } else {
-                echo "<div id='error'>A jelszavak nem egyeznek!</div>";
-            }
-        }
-
         ?>
 
-        <div class="container">
-            <h2 class="title">Jelszó megváltoztatása</h2>
-            <form id="form" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST">
-                <div class="input-cont">
-                    <img class="icon" src="/img/icons/pwd.svg" alt="password">
-                    <input name="pwd1" type="password" placeholder="Jelszó" required>
-                </div>
-                <div class="input-cont">
-                    <img class="icon" src="/img/icons/pwd.svg" alt="password">
-                    <input name="pwd2" type="password" placeholder="Jelszó újra" required>
-                </div>
-                <button name="submit" id="submit" type="submit">Változtat</button>
-            </form>
+        <div class="wrapper">
+            <?php
+            echo "<h1 class='title'>Üdvözöllek {$_SESSION["email"]}!</h1>";
+            if (isset($_POST["submit"])) {
+                $pwd1 = $_POST["pwd1"];
+                $pwd2 = $_POST["pwd2"];
+                if ($func->comparePwds($pwd1, $pwd2)) {
+                    if (!$func->isNewPwd($pwd1, $_SESSION["email"])) {
+                        echo "<div id='error'>Kérem adjon meg egy ÚJ jelszót!</div>";
+                    } else {
+                        if (!$func->changePwd($pwd1, $_SESSION["email"])) {
+                            echo "<div id='error'>Hiba történt!</div>";
+                        } else {
+                            echo "<div><p>Sikeresen megváltoztatta a jelszavát!</p>";
+                            header("refresh:10;url=../parts/logout.php");
+                            echo "<p>A rendszer 10 másodpercen belül kifogja léptetni!</p></div>";
+                        }
+                    }
+                } else {
+                    echo "<div id='error'>A jelszavak nem egyeznek!</div>";
+                }
+            }
+            ?>
+            <div class="wrapper">
+                <h2 class="title">Jelszó megváltoztatása</h2>
+                <form id="form" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST">
+                    <div class="input-cont">
+                        <img class="icon" src="/img/icons/pwd.svg" alt="password">
+                        <input name="pwd1" type="password" placeholder="Jelszó" required>
+                    </div>
+                    <div class="input-cont">
+                        <img class="icon" src="/img/icons/pwd.svg" alt="password">
+                        <input name="pwd2" type="password" placeholder="Jelszó újra" required>
+                    </div>
+                    <button name="submit" id="submit" type="submit">Változtat</button>
+                </form>
+            </div>
         </div>
     </main>
 </body>
